e5a361ea85453d481ec5b1d72ac8925fb3ccccde
Edvin Hultberg <hultberg@users.noreply.github.com>
2016-07-29 22:03:03 +0200

    Revert "Revert "Travis build""

--- src/assets/js/svg4everybody-amd.js
+++ src/assets/js/svg4everybody-amd.js
@@ -1,4 +1,8 @@
+/*jslint browser: true*/
+/*global define */
 // ref: https://github.com/jonathantneal/svg4everybody/issues/114#issuecomment-227437179
-define('svg4everybody-amd', ['svg4everybody'], function(svg4everybody) {
+define('svg4everybody-amd', ['svg4everybody'], function (svg4everybody) {
+  "use strict";
+
   svg4everybody();
 });
